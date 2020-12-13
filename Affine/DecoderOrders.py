ea53ca3e22078840eb9fd57ccba875827efc0da9
samironius <samirshahubs@gmail.com>
2020-12-13 03:10:56 +0200

    any order fix

--- Affine/DecoderOrders.py
+++ Affine/DecoderOrders.py
@@ -1,5 +1,4 @@
 import numpy as np
-import string
 
 
 def gcdex(a, b):
@@ -9,20 +8,17 @@ def gcdex(a, b):
     return x, y - (a // b) * x
 
 
-alphabet = string.ascii_lowercase
-count = 4
+count = 5
 keys = list(map(int, open("key.txt", "r").readline().split()))
-
 a, s, m = np.array(keys[-count-1::-1][::-1]).reshape(count, count), np.array(keys[-count:]).reshape(count, 1), 128
-print(a, s)
-print(np.gcd(int(np.linalg.det(a)), m))
 text = open("encoding_result.txt", "r").read()
 det = int(np.linalg.det(a))
 a1 = np.matrix.round((np.linalg.inv(a) * det * int(gcdex(det, m)[0])) % m)
 s1 = (-a1 @ s) % m
 x = np.array(list(map(ord, text))).reshape(len(a1), int(len(text)/count))
-print(x)
 zash_matrix = (a1 @ x + s1) % m
-print(zash_matrix)
 encod_result = "".join(list(map(chr, [round(char) for row in zash_matrix for char in row])))
+
 open("decoding_result.txt", "w").write(encod_result)
+print(x)
+print(zash_matrix)
--- Affine/Encoder_orders.py
+++ Affine/Encoder_orders.py
@@ -1,24 +1,22 @@
 import random
 import numpy as np
-import string
 
-alphabet = string.ascii_lowercase
-count = 4
+count = 5
+a, s, m = [[1, 2], [3, 4]], [[5], [6]], 128
+s = np.array([random.randint(0, m) for x in range(count)]).reshape(count, 1)
 text = open("text.txt", "r").read()
+
+while np.gcd(int(np.linalg.det(a)), m) != 1:
+    a = np.array([random.choice([x for x in range(m)]) for x in range(count*count)]).reshape(count, count)
+
 while len(text) % count != 0:
     text += "x"
 
-a, s, m = [[1, 2], [3, 4]], [[5], [6]], 128
-s = np.array([random.randint(0, m) for x in range(count)]).reshape(count, 1)
-print(s)
-while np.gcd(int(np.linalg.det(a)), m) != 1:
-    a = np.array([random.choice([x for x in range(m)]) for x in range(count*count)]).reshape(count, count)
-print(a)
-open("key.txt", "w").write(" ".join([str(char) for row in a for char in row]) + " " + " ".join([str(char) for row in s for char in row]))
 x = np.array(list(map(ord, text))).reshape(count, round(len(text)/count))
-print(x)
 zash_matrix = (a @ x + s) % m
-print(zash_matrix)
 encod_result = "".join(list(map(chr, [char for row in zash_matrix for char in row])))
 
+open("key.txt", "w").write(" ".join([str(char) for row in a for char in row]) + " " + " ".join([str(char) for row in s for char in row]))
 open("encoding_result.txt", "w").write(encod_result)
+print(x)
+print(zash_matrix)
