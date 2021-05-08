4e40fdcc631daa2655979a04ad56f390fe0bbaca
Samir <Samirshahub@gmail.com>
2021-05-09 00:39:13 +0300

    fix

--- Affine/DecoderOrders.py
+++ Affine/DecoderOrders.py
@@ -8,17 +8,15 @@ def gcdex(a, b):
     return x, y - (a // b) * x
 
 
-count = 7
-keys = list(map(int, open("key.txt", "r").readline().split()))
-a, s, m = np.array(keys[-count-1::-1][::-1]).reshape(count, count), np.array(keys[-count:]).reshape(count, 1), 128
-text = open("encoding_result.txt", "r").read()
-det = int(np.linalg.det(a))
-a1 = np.matrix.round((np.linalg.inv(a) * det * int(gcdex(det, m)[0])) % m)
-s1 = (-a1 @ s) % m
-x = np.array(list(map(ord, text))).reshape(len(a1), int(len(text)/count))
-zash_matrix = (a1 @ x + s1) % m
-encod_result = "".join(list(map(chr, [round(char) for row in zash_matrix for char in row])))
-
-open("decoding_result.txt", "w").write(encod_result)
-print(x)
-print(zash_matrix)
+def decoding():
+    count = 4
+    keys = list(map(int, open("key.txt", "r").readline().split()))
+    a, s, m = np.array(keys[-count-1::-1][::-1]).reshape(count, count), np.array(keys[-count:]).reshape(count, 1), 128
+    text = open("encoding_result.txt", "r").read()
+    det = int(np.linalg.det(a))
+    a1 = np.matrix.round((np.linalg.inv(a) * det * int(gcdex(det, m)[0])) % m)
+    s1 = (-a1 @ s) % m
+    x = np.array(list(map(ord, text))).reshape(len(a1), int(len(text)/count))
+    zash_matrix = (a1 @ x + s1) % m
+    decoded_result = "".join(list(map(chr, [round(char) for row in zash_matrix for char in row])))
+    open("decoding_result.txt", "w").write(decoded_result.strip("x"))
--- Affine/Encoder_orders.py
+++ Affine/Encoder_orders.py
@@ -1,23 +1,50 @@
 import random
 import numpy as np
+import DecoderOrders
 
-count = 7
-a, s, m = [[1, 2], [3, 4]], [[5], [6]], 128
-s = np.array([random.randint(0, m) for x in range(count)]).reshape(count, 1)
-text = open("text.txt", "r").read()
 
-while np.gcd(int(np.linalg.det(a)), m) != 1:
-    a = np.array([random.choice([x for x in range(m)]) for x in range(count*count)]).reshape(count, count)
-print(a)
+def encoding():
+    count = 4
+    a, s, m = [[1, 1], [1, 1]], [[1], [1]], 128
+    s = np.array([random.randint(0, m) for x in range(count)]).reshape(count, 1)
+    text = open("text.txt", "r").read()
+    while np.gcd(int(np.linalg.det(a)), m) != 1:
+        a = np.array([random.choice([x for x in range(m)]) for x in range(count*count)]).reshape(count, count)
+    for x in range(4 - len(text) % count):text += "x"
+    x = np.array(list(map(ord, text))).reshape(count, round(len(text)/count))
+    zash_matrix = (a @ x + s) % m
+    encod_result = "".join(list(map(chr, [char for row in zash_matrix for char in row])))
+    open("key.txt", "w").write(" ".join([str(char) for row in a for char in row]) + " " +
+                               " ".join([str(char) for row in s for char in row]))
+    open("encoding_result.txt", "w").write(encod_result)
+    DecoderOrders.decoding()
 
-while len(text) % count != 0:
-    text += "x"
 
-x = np.array(list(map(ord, text))).reshape(count, round(len(text)/count))
-zash_matrix = (a @ x + s) % m
-encod_result = "".join(list(map(chr, [char for row in zash_matrix for char in row])))
+encoding()
 
-open("key.txt", "w").write(" ".join([str(char) for row in a for char in row]) + " " + " ".join([str(char) for row in s for char in row]))
-open("encoding_result.txt", "w").write(encod_result)
-print(x)
-print(zash_matrix)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+while open("text.txt", "r").read() != open("decoding_result.txt", "r").read():
+    encoding()
