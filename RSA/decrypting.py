d5df8e7e42d6acb05a37d6f0dabfee761d720587
Samir <Samirshahub@gmail.com>
2021-05-11 02:32:59 +0300

    text encryption

--- RSA/decrypting.py
+++ RSA/decrypting.py
@@ -1,8 +1,10 @@
 def decrypting(letter):
-    return str((letter ** private_key) % open_key_n)
+    return str((int(letter) ** private_key) % n)
 
 
-open_keys = list(map(int, open("public_key.txt", "r").read().split()))
-text = list(map(int, open("encrypted.txt", "r").read().split(" ")))
-private_key, open_key_e, open_key_n = int(open("private_key.txt", "r").read()), open_keys[0], open_keys[1]
-open("decrypted.txt", "w").write("".join(list(map(decrypting, text))))
+open_keys = list(map(int, open("public_key", "r").read().split()))
+cod = list(map(int, open("encrypted", "r").read().split(" ")))
+private_key, e, n = int(open("private_key", "r").read()), open_keys[0], open_keys[1]
+splited_cod = list(map(int, map("".join, zip(*[iter(list(map(decrypting, cod)))] * 2))))
+open("decrypted", "w").write("".join(list(map(chr, splited_cod))))
+
--- RSA/encrypting.py
+++ RSA/encrypting.py
@@ -9,7 +9,7 @@ def gcdex(a, b):
 
 
 def encrypting(letter):
-    return str((letter ** public_key_e) % public_key_n)
+    return str((int(letter) ** public_key_e) % public_key_n)
 
 
 p, q = 3, 13
@@ -17,6 +17,7 @@ el = (p - 1) * (q - 1)
 public_key_n = p * q
 public_key_e = random.choice([x for x in range(0, el) if gcdex(el, x)[0] == 1])
 private_key = gcdex(el, public_key_e)[2] % el
-open("encrypted.txt", "w").write(" ".join(list(map(encrypting, list(map(int, open("text.txt", "r").read()))))))
-open("public_key.txt", "w").write("%s %s" % (str(public_key_e), str(public_key_n)))
-open("private_key.txt", "w").write(str(private_key))
+text = "".join(list(map(str, map(ord, open("text", "r").read()))))
+open("encrypted", "w").write(" ".join(list(map(encrypting, text))))
+open("public_key", "w").write("%s %s" % (str(public_key_e), str(public_key_n)))
+open("private_key", "w").write(str(private_key))
