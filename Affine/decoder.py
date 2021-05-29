021356c3d30b57752e69d912b804ca43bd608d2c
Samir <Samirshahub@gmail.com>
2021-05-29 17:43:30 +0300

    cipher in one file

--- Affine/decoder.py
+++ Affine/decoder.py
@@ -1,17 +1,17 @@
-def gcdex(a, b):
-    if b == 0:
-        return 1, 0
-    y, x = gcdex(b, a % b)
-    return x, y - (a // b) * x
+from egcd import egcd
 
 
 def decrypt(char):
     return chr((a1 * ord(char) + s1) % m)
 
 
+def encrypt(char):
+    return chr((a * ord(char) + s) % m)
+
+
 a, s, m = map(int, open("key.txt", "r").read().split())
-a1 = gcdex(a, m)[0]
+a1 = egcd(a, m)[1]
 s1 = (-a1 * s) % m
 
-
+open("encoding_result.txt", "w").write("".join(map(encrypt, open("text.txt", "r").read())))
 open("decoding_result.txt", "w").write("".join(map(decrypt, open("encoding_result.txt", "r").read())))
